1e1c0d936fd82e816607bc2f7b8b0dd65be9c88c
Nikola Lakic <nikola@lakic.one>
2020-10-23 00:24:13 +0200

    Inicijalizacija za zavareni profil se sada nalazi u konstruktoru

--- MetalneKonstrukcije/MetalOOP.py
+++ MetalneKonstrukcije/MetalOOP.py
@@ -18,22 +18,37 @@ class Profil:
             self.indeks = indeks
             self.lista_profila = lista_profila
         elif ime_profila == 'ZAVAREN':
-            pass
+            h = float(input('Unesi visinu profila h [mm]: '))
+            h = h/1000  # [m]
+            self.h = h
+            b1 = float(input('Unesi sirinu pritisnute nozice b1 [mm]: '))
+            b1 = b1/1000  # [m]
+            b2 = float(input('Unesi sirinu zategnute nozice b2 [mm]: '))
+            b2 = b2/1000
+            self.b1 = b1
+            self.b2 = b2
+            tf1 = float(input('Unesi debljinu pritisnute nozice tf1 [mm]: '))
+            tf1 = tf1/1000  # [m]
+            tf2 = float(input('Unesi debljinu zategnute nozice tf2 [mm]: '))
+            tf2 = tf2/1000
+            self.tf1 = tf1
+            self.tf2 = tf2
+            tw = float(input('Unesi debljinu rebra tw [mm]: '))
+            tw = tw/1000
+            self.tw = tw
+            aw = float(input('Unesi debljinu vara izmedju nozice i rebra aw [mm]: '))
+            aw = aw/1000
+            self.aw = aw
+            hi = h - tf1 - tf2 - 2*aw*math.sqrt(2)
+            self.hi = hi
         else:
             print('\nIme profila mora biti iz kolone profili ili "zavaren"!')
             exit()
 
-
-class GeometrijaDimenzije(Profil):
     def h(self):
         if self.ime_profila in self.lista_profila:
             h_niz = self.df['h [mm]'].to_numpy()*math.pow(10, -3)  # [m]
             h = h_niz[self.indeks]
-        elif self.ime_profila == 'ZAVAREN':
-            h = float(input('Unesi visinu profila h [mm]: '))
-            h = h/1000  # [m]
-        else:
-            h = False
         return h
 
     def b(self):
@@ -42,14 +57,6 @@ class GeometrijaDimenzije(Profil):
             b = b_niz[self.indeks]
             b1 = b
             b2 = b
-        elif self.ime_profila == 'ZAVAREN':
-            b1 = float(input('Unesi sirinu pritisnute nozice b1 [mm]: '))
-            b1 = b1/1000  # [m]
-            b2 = float(input('Unesi sirinu zategnute nozice b2 [mm]: '))
-            b2 = b2/1000
-        else:
-            b1 = False
-            b2 = False
         b_niz = np.array([b1, b2])
         return b_niz
 
@@ -59,14 +66,6 @@ class GeometrijaDimenzije(Profil):
             tf = tf_niz[self.indeks]
             tf1 = tf
             tf2 = tf
-        elif self.ime_profila == 'ZAVAREN':
-            tf1 = float(input('Unesi debljinu pritisnute nozice tf1 [mm]: '))
-            tf1 = tf1/1000  # [m]
-            tf2 = float(input('Unesi debljinu zategnute nozice tf2 [mm]: '))
-            tf2 = tf2/1000
-        else:
-            tf1 = False
-            tf2 = False
         tf_niz = np.array([tf1, tf2])
         return tf_niz
 
@@ -74,9 +73,12 @@ class GeometrijaDimenzije(Profil):
         if self.ime_profila in self.lista_profila:
             tw_niz = self.df['tw [mm]'].to_numpy() * math.pow(10, -3)  # [m]
             tw = tw_niz[self.indeks]
-        elif self.ime_profila == 'ZAVAREN':
-            tw = float(input('Unesi debljinu rebra tw [mm]: '))
-            tw = tw/1000
-        else:
-            tw = False
         return tw
+
+    def hi(self):
+        if self.ime_profila in self.lista_profila:
+            hi_niz = self.df['hi [mm]'].to_numpy() * math.pow(10, -3)  # [m]
+            hi = hi_niz[self.indeks]
+            return hi
+
+dimenzija = Profil()
